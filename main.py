f7645559ea0ad2e6b7a2b016f2341ad9b7d81d71
tundut <ducnguyenphan05@gmail.com>
2024-10-07 20:33:49 +0700

    fix bug

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 
-load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
